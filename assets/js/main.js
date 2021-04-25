2d6161c5999816b6995acf266523ef31337f3b2d
Mario Mamdouh <mariomamdouh237@gmail.com>
2021-04-25 18:23:08 +0200

    Add Home Animation

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,33 @@
 //Define navbar icon
 var navMenuIcon = document.getElementById('navMenuIcon');
 //add event click to navbar toggle
-navMenuIcon.addEventListener('click' , toggleMenu);
+navMenuIcon.addEventListener('click', toggleMenu);
 
 
 //function to open or close navbar 
-function toggleMenu(){
+function toggleMenu() {
     let toggle = document.querySelector('.toggle');
     let navList = document.querySelector('.navbar');
 
     toggle.classList.toggle('active');
     navList.classList.toggle('active');
 }
+
+
+// Init AOS Animation
+function aos_init() {
+    AOS.init({
+        duration: 1000,
+        once: true
+    });
+};
+
+
+//Call Aos Init when page load
+$(window).on('load', function () {
+    aos_init();
+});
+
+
+
 
