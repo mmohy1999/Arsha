110bf71b9465d4d6e10b7805d5a513389da43c57
Mario Mamdouh <mariomamdouh237@gmail.com>
2021-04-28 12:10:26 +0200

    Modify navbar events
    1. Change javascript code to jquery
    2. Change navbar background color when scroll
    3. Active navbar anchor when section is in viewport
    4. Scroll To section when anchor clicked with smooth animation

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,77 +1,106 @@
-//Define navbar icon
-var navMenuIcon = document.getElementById('navMenuIcon');
-//Define Scroll To Top Btn
-var scrollBtn = document.querySelector('.scroll-to-top');
-
-// Define collapse list
-var collapseList = Array.from(document.querySelectorAll('#sec-collapse .collapse-list button'));
-
-
 //Hide Loading when page is loaded
 $(window).on('load', () => {
     if ($('.page-load').length) {
-      $('.page-load').delay(100).fadeOut('slow', ()=> {
-        $(this).remove();
-      });
+        $('.page-load').delay(100).fadeOut('slow', () => {
+            $(this).remove();
+        });
     }
-  });
+});
 
-//add event click to navbar toggle
-navMenuIcon.addEventListener('click', toggleMenu);
-
-//add event on scroll to scroll to top btn
-window.onscroll = function () { showScrollToTop() };
-
-//Function to show or hide scroll to top btn
-function showScrollToTop() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        scrollBtn.style.display = "flex";
-    } else {
-        scrollBtn.style.display = "none";
-    }
-}
-
-
-//Jquery onclick action to scroll to top smoothly
-
-$(".scroll-to-top").click(function () {
-    $('html, body').animate({
-        scrollTop: 0
-    }, 1500);
+//open or close navbar when icon clicked
+$('#navMenuIcon').on('click', () => {
+    $('.toggle').toggleClass('active');
+    $('.navbar').toggleClass('active');
 });
 
 
-//function to open or close navbar 
-function toggleMenu() {
-    let toggle = document.querySelector('.toggle');
-    let navList = document.querySelector('.navbar');
 
-    toggle.classList.toggle('active');
-    navList.classList.toggle('active');
+//show or hide scroll to top btn & change header background color
+$(window).on('scroll' , () => {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        $('.scroll-to-top').css('display', 'flex');
+        $('header').css('background', 'rgba(40, 58, 90, 0.9)');
+    } else {
+        $('.scroll-to-top').css('display', 'none');
+        $('header').css('background', '#37517e');
+
+    }
+});
+
+
+//active navbar Anchors when scroll
+
+const listOfSections = $('section');
+
+$(window).on('scroll',()=> {
+
+    let currentPosition = $(this).scrollTop() + 220;
+
+    listOfSections.each(function () {
+        let sectionTop = $(this).offset().top,
+            sectionBottom = sectionTop + $(this).outerHeight();
+
+        if (currentPosition >= sectionTop && currentPosition <= sectionBottom) {
+            if (currentPosition <= sectionBottom) {
+                $('.navbar ul li a').removeClass('active');
+            }
+            $('a[href="#' + $(this).attr('id') + '"]').addClass('active');
+        }
+        
+    })
 }
+) 
+
+
+//scroll To Section when nav anchor clicked
+$('.navbar ul li a').on('click', function(event){
+    event.preventDefault(); 
+    
+    let sectionID = $(this).attr('href');
+
+    let sectionPosition = $(sectionID).offset().top - 75;
+
+
+    //hide navbar in mobile and change navbar icon
+    $('.navbar').removeClass('active');
+    $('.toggle').removeClass('active');
+
+    //scroll to section smoothly
+    $('html , body').animate({
+        scrollTop: sectionPosition
+    },1100);
+    
+   
+})
+
+
+
+//scroll to top smoothly
+$(".scroll-to-top").click(() => {
+    $('html, body').animate({
+        scrollTop: 0
+    }, 1100);
+});
 
 
 // Init AOS Animation
-function aos_init() {
+$(window).on('load', () => {
     AOS.init({
         duration: 1500,
         once: true
     });
-};
-
-
-//Call Aos Init when page load
-$(window).on('load', function () {
-    aos_init();
 });
 
 
+
+// Define collapse list
+var collapseList = Array.from(document.querySelectorAll('#sec-collapse .collapse-list button'));
 
 // onclick on collapse btn
 for (let i = 0; i < collapseList.length; i++) {
     collapseList[i].addEventListener('click', () => {
         let collapseIcon = collapseList[i].querySelector('i');
-        if ( collapseList[i].classList.contains('collapsed'))
+        if (collapseList[i].classList.contains('collapsed'))
             collapseIcon.classList.replace('fa-sort-down', 'fa-sort-up')
         else
             collapseIcon.classList.replace('fa-sort-up', 'fa-sort-down')
